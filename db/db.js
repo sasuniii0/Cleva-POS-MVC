bf21b25398b31e5bbb0e91fd9fc57982627e5b0f
sasuniii0 <sasuniwijerathne@gmail.com>
2025-05-21 12:45:43 +0530

    finalized

--- db/db.js
+++ db/db.js
@@ -1,9 +1,3 @@
 export let customer_db = [];
 export let item_db = [];
 export let order_db = [];
-
-export function saveAllDatabases() {
-    localStorage.setItem('customer_db', JSON.stringify(customer_db));
-    localStorage.setItem('item_db', JSON.stringify(item_db));
-    localStorage.setItem('order_db', JSON.stringify(order_db));
-}
